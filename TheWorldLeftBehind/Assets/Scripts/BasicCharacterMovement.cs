516689398974c5934de51372e9c2a74f198ad276
Zach Miller <zpm346@gmail.com>
2018-05-10 17:14:16 -0500

    Update 01
    -Added basic character with controls
    -Added basic map with ProBuilder

---
+++ TheWorldLeftBehind/Assets/Scripts/BasicCharacterMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicCharacterMovement : MonoBehaviour
+{
+
+	public float movementSpeed = 0f;
+
+	private float checkIf0 = 0f;
+
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal");
+		var y = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(x, 0.0f, y);
+
+		if(movement != Vector3.zero) //Check if V3 is 0
+			transform.rotation = Quaternion.LookRotation(movement);
+
+		transform.Translate (movement * movementSpeed * Time.deltaTime, Space.World);
+	}
+
+}
---
+++ TheWorldLeftBehind/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform target;
+	public float distance1;
+	public float distance2;
+	public float height;
+	void Update(){
+		Vector3 temp = transform.position;
+
+
+		temp.z = target.position.z - distance1;
+		temp.y = target.position.y + height;
+		temp.x = target.position.x + distance2;
+
+		transform.position = temp;
+	}
+}
